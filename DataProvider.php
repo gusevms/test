08af30f6aa690cb7c5c48c3b533f4ed19ffc0a35
gusevms <53150404+gusevms@users.noreply.github.com>
2019-07-21 20:20:57 +0300

    Update DataProvider.php

--- DataProvider.php
+++ DataProvider.php
@@ -3,28 +3,28 @@ namespace src\Integration;
 class DataProvider
 {
     private $host;
-	private $user;
-	private $password;
+    private $user;
+    private $password;
 	
-	/**     
-	 * @param $host     
-	 * @param $user     
-	 * @param $password     
-	 */
-	public function __construct($host, $user, $password)    
-	{
-		$this->host = $host;
-		$this->user = $user;
-		$this->password = $password;
-	}
-	
-	/**
-	 * @param array $request     
-	 *     
-	 * @return array     
-	 */
-	public function get(array &$request)
+    /**     
+     * @param $host     
+     * @param $user     
+     * @param $password     
+     */
+    public function __construct($host, $user, $password)    
     {
-		// returns a response from external service    
-	}
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+    }
+	
+    /**
+     * @param array $request     
+     *     
+     * @return array     
+     */
+    public function get(array &$request)
+    {
+        // returns a response from external service    
+    }
 }
